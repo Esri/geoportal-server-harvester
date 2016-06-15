18f13e45d1365e3b328f354e802194798c537b21
pete5162 <pandzel@esri.com>
2016-06-15 13:27:14 -0700

    Robots test unit.

---
+++ geoportal-commons/geoportal-commons-robots/src/test/java/com/esri/geoportal/commons/robots/BotsTest.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2016 Esri, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.esri.geoportal.commons.robots;
+
+import java.io.IOException;
+import java.io.InputStream;
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Rule;
+import org.mockserver.client.server.MockServerClient;
+import org.mockserver.junit.MockServerRule;
+import static org.mockserver.model.HttpRequest.request;
+import static org.mockserver.model.HttpResponse.response;
+
+/**
+ * Bots test.
+ */
+public class BotsTest {
+
+  @Rule
+  public MockServerRule server = new MockServerRule(this,5000);
+  
+  private static MockServerClient client;
+
+  @Before
+  public void setupClass() throws IOException {
+    client = new MockServerClient("localhost", 5000);
+    client.when(
+                    request()
+                    .withMethod("GET")
+                    .withPath("/robots.txt")
+            )
+            .respond(
+                    response()
+                    .withStatusCode(200)
+                    .withBody(readAndClose(getClass().getResourceAsStream("/robots.txt")))
+            );
+  }
+
+  @Test
+  public void testAccessGranted() throws IOException {
+    Bots bots = BotsUtils.readBots(BotsMode.always, "http://localhost:5000/robots.txt");
+    Access access = BotsUtils.requestAccess(bots, "http://localhost:5000/data.txt");
+    
+    assertTrue("Access denied", access.hasAccess());
+  }
+
+  @Test
+  public void testAccessDenied() throws IOException {
+    Bots bots = BotsUtils.readBots(BotsMode.always, "http://localhost:5000/robots.txt");
+    Access access = BotsUtils.requestAccess(bots, "http://localhost:5000/tmp/data.txt");
+    
+    assertFalse("Access granted", access.hasAccess());
+  }
+
+  @Test
+  public void testAccessIgnored() throws IOException {
+    Bots bots = BotsUtils.readBots(BotsMode.never, "http://localhost:5000/robots.txt");
+    Access access = BotsUtils.requestAccess(bots, "http://localhost:5000/tmp/data.txt");
+    
+    assertTrue("Access denied", access.hasAccess());
+  }
+
+  @Test
+  public void testAccessInapplicable() throws IOException {
+    Bots bots = BotsUtils.readBots(BotsMode.always, "http://localhost:5000/robots.txt");
+    Access access = BotsUtils.requestAccess(bots, "http://localhost:5000/test/data.txt");
+    
+    assertTrue("Access denied", access.hasAccess());
+  }
+
+  private String readAndClose(InputStream in) throws IOException {
+    try (InputStream input=in) {
+      return IOUtils.toString(input, "UTF-8");
+    }
+  }
+  
+}
