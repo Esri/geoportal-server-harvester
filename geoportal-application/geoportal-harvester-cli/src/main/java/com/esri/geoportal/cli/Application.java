769dd79f5f2831e8d7231abcd2f13cdcb1849013
pete5162 <pandzel@esri.com>
2016-10-26 16:25:18 -0700

    Cli skeleton.

--- geoportal-application/geoportal-harvester-cli/src/main/java/com/esri/geoportal/cli/Application.java
+++ geoportal-application/geoportal-harvester-cli/src/main/java/com/esri/geoportal/cli/Application.java
@@ -17,8 +17,16 @@ package com.esri.geoportal.cli;
 
 import com.esri.geoportal.cli.boot.Bootstrap;
 import com.esri.geoportal.harvester.api.base.SimpleIteratorContext;
+import com.esri.geoportal.harvester.api.defs.TaskDefinition;
+import com.esri.geoportal.harvester.api.ex.DataProcessorException;
+import com.esri.geoportal.harvester.api.ex.InvalidDefinitionException;
 import com.esri.geoportal.harvester.api.specs.InputBroker.IteratorContext;
 import com.esri.geoportal.harvester.engine.services.Engine;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 
 /**
  * Command line application.
@@ -26,8 +34,26 @@ import com.esri.geoportal.harvester.engine.services.Engine;
 public class Application {
   
   public static void main(String[] args) throws Exception {
+    Application app = new Application();
+    app.execute(args);
+  }
+  
+  public void execute(String [] args) {
+    
+  }
+  
+  protected CommandLine parseArgs(String [] args) throws ParseException {
+    Options options = new Options();
+    
+    CommandLineParser parser = new DefaultParser();
+    return parser.parse(options, args);
+  }
+  
+  protected void harvest(TaskDefinition taskDefinition) throws DataProcessorException, InvalidDefinitionException {
     Bootstrap boot = new Bootstrap();
     Engine engine = boot.createEngine();
     IteratorContext iterCtx = new SimpleIteratorContext();
+    
+    engine.getExecutionService().execute(taskDefinition, iterCtx);
   }
 }
