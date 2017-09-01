6ea42ce7b9764ea21e2129dd0fd8d57dd728def8
Piotr Andzel <pandzel@esri.com>
2017-09-01 11:40:35 -0700

    CKAN: data.gov downloading metadata.

--- geoportal-connectors/geoportal-harvester-ckan/src/main/java/com/esri/geoportal/harvester/ckan/data/gov/DataGovBroker.java
+++ geoportal-connectors/geoportal-harvester-ckan/src/main/java/com/esri/geoportal/harvester/ckan/data/gov/DataGovBroker.java
@@ -15,12 +15,24 @@
  */
 package com.esri.geoportal.harvester.ckan.data.gov;
 
+import com.esri.geoportal.commons.ckan.client.Dataset;
+import com.esri.geoportal.commons.ckan.client.Extra;
+import com.esri.geoportal.commons.ckan.client.Response;
 import com.esri.geoportal.commons.meta.MetaBuilder;
 import com.esri.geoportal.harvester.api.defs.EntityDefinition;
+import com.esri.geoportal.harvester.api.ex.DataInputException;
 import com.esri.geoportal.harvester.api.specs.InputConnector;
 import com.esri.geoportal.harvester.ckan.CkanBroker;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
+import org.apache.commons.io.IOUtils;
+import org.apache.http.client.HttpResponseException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -63,5 +75,39 @@ import org.slf4j.LoggerFactory;
   @Override
   public EntityDefinition getEntityDefinition() {
     return definition.getEntityDefinition();
+  }
+
+  @Override
+  protected String createDocument(Dataset dataSet) throws DataInputException {
+    if (dataSet.extras!=null) {
+      String oid = findOid(dataSet);
+      if (oid!=null) {
+        try {
+          URL xmlUrl = makeXmlUrl(oid);
+          HttpGet req = new HttpGet(xmlUrl.toURI());
+          try (CloseableHttpResponse httpResponse = httpClient.execute(req); InputStream contentStream = httpResponse.getEntity().getContent();) {
+            String reasonMessage = httpResponse.getStatusLine().getReasonPhrase();
+            String responseContent = IOUtils.toString(contentStream, "UTF-8");
+            LOG.trace(String.format("RESPONSE: %s, %s", responseContent, reasonMessage));
+            return responseContent;
+          }
+        } catch(URISyntaxException|IOException ex) {
+          throw new DataInputException(this, String.format("Error reading metadata for object: %s", oid), ex);
+        }
+      }
+    }
+    return super.createDocument(dataSet);
+  }
+  
+  private String findOid(Dataset dataSet) {
+    return dataSet.extras.stream()
+              .filter((Extra extra)->extra.key.equals(definition.getOidKey()) && extra.value instanceof String)
+              .map((Extra extra)->(String)extra.value)
+              .findFirst()
+              .orElse(null);
+  }
+  
+  private URL makeXmlUrl(String oid) throws MalformedURLException {
+    return new URL(definition.getXmlUrl().toExternalForm().replaceAll("/+$", "") + "/" + oid);
   }
 }
