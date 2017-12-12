72b5beb9ff9d2ec6edd91bf3cc8f46524bffce3a
pandzel <pandzel@esri.com>
2017-12-12 10:48:59 -0800

    listPublicContent() method defined in AgpClient.

--- geoportal-commons/geoportal-commons-agp-client/src/main/java/com/esri/geoportal/commons/agp/client/AgpClient.java
+++ geoportal-commons/geoportal-commons-agp-client/src/main/java/com/esri/geoportal/commons/agp/client/AgpClient.java
@@ -356,6 +356,34 @@ public class AgpClient implements Closeable {
   }
   
   /**
+   * Lists public content. Only specified item types will be included. See config.properties file.
+   * @param num number items to return
+   * @param start start item
+   * @param token token (optional)
+   * @return content response
+   * @throws URISyntaxException if invalid URL
+   * @throws IOException if operation fails
+   */
+  public ContentResponse listPublicContent(long num, long start, String token) throws URISyntaxException, IOException {
+    URIBuilder builder = new URIBuilder(searchUri());
+    
+    builder.setParameter("f", "json");
+    builder.setParameter("num", Long.toString(num));
+    builder.setParameter("start", Long.toString(start));
+    if (token!=null) {
+      builder.setParameter("token", token);
+    }
+    
+    String type = Arrays.stream(Config.readTypes()).map(s->StringUtils.trimToNull(s)).filter(s->s != null).collect(Collectors.joining(" OR "));
+    String q = String.format("type: (%s)", type);
+    builder.setParameter("q", q);
+    
+    HttpGet req = new HttpGet(builder.build());
+    
+    return execute(req,ContentResponse.class);
+  }
+  
+  /**
    * Lists folders.
    * @param owner owner
    * @param token token
