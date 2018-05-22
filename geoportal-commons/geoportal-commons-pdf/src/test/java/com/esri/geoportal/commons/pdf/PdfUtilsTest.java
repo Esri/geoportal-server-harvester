3b112bce35caed0f3592a8f0472d51e945428471
dandinma <mdandini@esri.com>
2018-05-22 17:15:07 -0400

    Added XML generation test

--- geoportal-commons/geoportal-commons-pdf/src/test/java/com/esri/geoportal/commons/pdf/PdfUtilsTest.java
+++ geoportal-commons/geoportal-commons-pdf/src/test/java/com/esri/geoportal/commons/pdf/PdfUtilsTest.java
@@ -7,7 +7,10 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.util.Properties;
 
+import com.esri.geoportal.commons.utils.XmlUtils;
+
 import org.junit.Test;
+import org.w3c.dom.Document;
 
 public class PdfUtilsTest {
     
@@ -34,6 +37,35 @@ public class PdfUtilsTest {
         assertEquals("wrong title", "MANAmap1", props.getProperty(PdfUtils.PROP_TITLE));
         assertEquals("wrong description", "\nAuthor: null\nCreator: Adobe Illustrator CS5\nProducer: Adobe PDF library 9.90", props.getProperty(PdfUtils.PROP_SUBJECT));
         assertEquals("Wrong bbox", "-77.54026794433594 38.815128326416016, -77.5213394165039 38.82757568359375", props.getProperty(PdfUtils.PROP_BBOX));
+    }
+
+    @Test
+    public void testGenerateMetadataXML() throws Exception {
+        byte[] bytes = getBytes("src/test/data/MANAmap1.pdf");
+
+        byte[] xmlBytes = PdfUtils.generateMetadataXML(bytes, "MANAmap1.pdf", "http://www.esri.com");
+
+        assertNotNull("Null bytes returned", xmlBytes);
+
+        Document doc = XmlUtils.toDocument(new String(xmlBytes));
+
+        assertNotNull("Null title", doc.getElementsByTagNameNS("http://purl.org/dc/elements/1.1/", "title").item(0));
+        assertEquals("Wrong title", "MANAmap1", doc.getElementsByTagNameNS("http://purl.org/dc/elements/1.1/", "title").item(0).getTextContent().trim());
+
+        assertNotNull("Null description", doc.getElementsByTagNameNS("http://purl.org/dc/elements/1.1/", "description").item(0));
+        assertEquals("Wrong description", "Author: null\nCreator: Adobe Illustrator CS5\nProducer: Adobe PDF library 9.90", doc.getElementsByTagNameNS("http://purl.org/dc/elements/1.1/", "description").item(0).getTextContent().trim());
+
+        assertNotNull("Null abstract", doc.getElementsByTagNameNS("http://purl.org/dc/terms/", "abstract").item(0));
+        assertEquals("Wrong abstract", "Author: null\nCreator: Adobe Illustrator CS5\nProducer: Adobe PDF library 9.90", doc.getElementsByTagNameNS("http://purl.org/dc/terms/", "abstract").item(0).getTextContent().trim());
+
+        assertNotNull("Null references", doc.getElementsByTagNameNS("http://purl.org/dc/terms/", "references").item(0));
+        assertEquals("Wrong references", "http://www.esri.com", doc.getElementsByTagNameNS("http://purl.org/dc/terms/", "references").item(0).getTextContent().trim());
+
+        assertNotNull("Null LowerCorner", doc.getElementsByTagNameNS("http://www.opengis.net/ows", "LowerCorner").item(0));
+        assertEquals("Wrong LowerCorner", "-77.54026794433594 38.815128326416016", doc.getElementsByTagNameNS("http://www.opengis.net/ows", "LowerCorner").item(0).getTextContent().trim());
+
+        assertNotNull("Null UpperCorner", doc.getElementsByTagNameNS("http://www.opengis.net/ows", "UpperCorner").item(0));
+        assertEquals("Wrong UpperCorner", "-77.5213394165039 38.82757568359375", doc.getElementsByTagNameNS("http://www.opengis.net/ows", "UpperCorner").item(0).getTextContent().trim());
     }
 
     private byte[] getBytes(String path) throws IOException{
