276a2172e96cb589c54b95eeec447eb460cc7a7f
dandinma <mdandini@esri.com>
2018-05-22 16:28:10 -0400

    Adding tests
    parsing geospatial PDF

--- geoportal-commons/geoportal-commons-pdf/src/test/java/com/esri/geoportal/commons/pdf/PdfUtilsTest.java
+++ geoportal-commons/geoportal-commons-pdf/src/test/java/com/esri/geoportal/commons/pdf/PdfUtilsTest.java
@@ -3,6 +3,7 @@ package com.esri.geoportal.commons.pdf;
 import static org.junit.Assert.*;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.util.Properties;
 
@@ -13,9 +14,8 @@ public class PdfUtilsTest {
     private static final String DEFAULT_GEOM = "https://utility.arcgisonline.com/ArcGIS/rest/services/Geometry/GeometryServer";
 
     @Test
-    public void testReadMetadata_readGeospatialPDF () throws Exception {
-        File pdf = new File("src/test/data/geoeyeuscapitalimage.pdf");
-        byte[] bytes = Files.readAllBytes(pdf.toPath());
+    public void testReadMetadata_readGeoPDF_UT () throws Exception {
+        byte[] bytes = getBytes("src/test/data/geoeyeuscapitalimage.pdf");
 
         Properties props = PdfUtils.readMetadata(bytes, "defaultTitle", DEFAULT_GEOM);
         
@@ -23,5 +23,21 @@ public class PdfUtilsTest {
         assertEquals("wrong title", "defaultTitle", props.getProperty(PdfUtils.PROP_TITLE));
         assertEquals("wrong description", "\nAuthor: null\nCreator: Adobe Acrobat 9.0\nProducer: Adobe Acrobat 9.0 Image Conversion Plug-in", props.getProperty(PdfUtils.PROP_SUBJECT));
         assertEquals("Wrong bbox", "-77.05301318703646 38.88323180425436, -77.00746902033382 38.898891017940734", props.getProperty(PdfUtils.PROP_BBOX));
+    }
+
+    @Test
+    public void testReadMetadata_readGeospatialPDF() throws Exception {
+        byte[] bytes = getBytes("src/test/data/MANAmap1.pdf");
+
+        Properties props = PdfUtils.readMetadata(bytes, "defaultTitle", DEFAULT_GEOM);
+        assertNotNull("null value returned", props);
+        assertEquals("wrong title", "MANAmap1", props.getProperty(PdfUtils.PROP_TITLE));
+        assertEquals("wrong description", "\nAuthor: null\nCreator: Adobe Illustrator CS5\nProducer: Adobe PDF library 9.90", props.getProperty(PdfUtils.PROP_SUBJECT));
+        assertEquals("Wrong bbox", "-77.54026794433594 38.815128326416016, -77.5213394165039 38.82757568359375", props.getProperty(PdfUtils.PROP_BBOX));
+    }
+
+    private byte[] getBytes(String path) throws IOException{
+        File pdf = new File(path);
+        return Files.readAllBytes(pdf.toPath());
     }
 }
