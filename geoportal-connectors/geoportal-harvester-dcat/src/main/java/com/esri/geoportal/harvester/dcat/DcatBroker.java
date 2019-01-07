ee91d78a838e59e7229d8fc4582003a2506e0ffc
pandzel <pandzel@esri.com>
2019-01-07 11:22:48 -0800

    Generic code template for DCAT input broker.

---
+++ geoportal-connectors/geoportal-harvester-dcat/src/main/java/com/esri/geoportal/harvester/dcat/DcatBroker.java
@@ -0,0 +1,137 @@
+/*
+ * Copyright 2016 Esri, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.esri.geoportal.harvester.dcat;
+
+import com.esri.geoportal.commons.http.BotsHttpClient;
+import com.esri.geoportal.commons.meta.MetaBuilder;
+import com.esri.geoportal.commons.robots.Bots;
+import com.esri.geoportal.commons.robots.BotsUtils;
+import com.esri.geoportal.commons.utils.SimpleCredentials;
+import com.esri.geoportal.harvester.api.defs.EntityDefinition;
+import com.esri.geoportal.harvester.api.ex.DataInputException;
+import com.esri.geoportal.harvester.api.ex.DataProcessorException;
+import com.esri.geoportal.harvester.api.specs.InputBroker;
+import com.esri.geoportal.harvester.api.specs.InputConnector;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.esri.geoportal.harvester.api.DataContent;
+import com.esri.geoportal.harvester.api.defs.TaskDefinition;
+
+/**
+ * CKAN broker.
+ */
+/*package*/ class DcatBroker implements InputBroker {
+
+  private static final Logger LOG = LoggerFactory.getLogger(DcatBroker.class);
+
+  private final DcatConnector connector;
+  private final DcatBrokerDefinitionAdaptor definition;
+  private final MetaBuilder metaBuilder;
+
+  protected CloseableHttpClient httpClient;
+  protected TaskDefinition td;
+
+  private static final ObjectMapper mapper = new ObjectMapper();
+
+  static {
+    mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    mapper.configure(JsonParser.Feature.ALLOW_NON_NUMERIC_NUMBERS, true);
+    mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+  }
+
+  /**
+   * Creates instance of the broker.
+   *
+   * @param connector connector
+   * @param definition definition
+   * @param metaBuilder meta builder
+   */
+  public DcatBroker(DcatConnector connector, DcatBrokerDefinitionAdaptor definition, MetaBuilder metaBuilder) {
+    this.connector = connector;
+    this.definition = definition;
+    this.metaBuilder = metaBuilder;
+  }
+
+  @Override
+  public void initialize(InitContext context) throws DataProcessorException {
+    definition.override(context.getParams());
+    td = context.getTask().getTaskDefinition();
+    CloseableHttpClient http = HttpClientBuilder.create().useSystemProperties().build();
+    if (context.getTask().getTaskDefinition().isIgnoreRobotsTxt()) {
+      httpClient = http;
+    } else {
+      Bots bots = BotsUtils.readBots(definition.getBotsConfig(), http, definition.getHostUrl());
+      httpClient = new BotsHttpClient(http, bots);
+    }
+  }
+
+  @Override
+  public void terminate() {
+    if (httpClient != null) {
+      try {
+        httpClient.close();
+      } catch (IOException ex) {
+        LOG.error(String.format("Error terminating broker."), ex);
+      }
+    }
+  }
+
+  @Override
+  public URI getBrokerUri() throws URISyntaxException {
+    return new URI("DCAT", definition.getHostUrl().toExternalForm(), null);
+  }
+
+  @Override
+  public Iterator iterator(IteratorContext iteratorContext) throws DataInputException {
+    // TODO: provide DCAT iterator implementation
+    return null;
+  }
+
+  @Override
+  public boolean hasAccess(SimpleCredentials creds) {
+    return true;
+  }
+
+  @Override
+  public String toString() {
+    return String.format("CKAN [%s]", definition.getHostUrl());
+  }
+
+  @Override
+  public InputConnector getConnector() {
+    return connector;
+  }
+
+  @Override
+  public EntityDefinition getEntityDefinition() {
+    return definition.getEntityDefinition();
+  }
+
+  @Override
+  public DataContent readContent(String id) throws DataInputException {
+    // TODO: provide DCAT iterator implementation
+    return null;
+  }
+}
