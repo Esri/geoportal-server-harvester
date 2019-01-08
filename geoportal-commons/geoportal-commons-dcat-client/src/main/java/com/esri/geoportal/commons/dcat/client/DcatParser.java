1bb1bc50c71b87a53857ccba71ed95583d61ba3b
pandzel <pandzel@esri.com>
2019-01-08 11:22:26 -0800

    DCAT parser updated.

--- geoportal-commons/geoportal-commons-dcat-client/src/main/java/com/esri/geoportal/commons/dcat/client/DcatParser.java
+++ geoportal-commons/geoportal-commons-dcat-client/src/main/java/com/esri/geoportal/commons/dcat/client/DcatParser.java
@@ -29,7 +29,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
