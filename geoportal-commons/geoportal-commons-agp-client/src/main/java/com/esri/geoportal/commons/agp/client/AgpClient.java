a10c3d63096af3c3c4346358a21e740cc451528e
pandzel <pandzel@esri.com>
2019-01-23 08:47:15 -0800

    Basic file upload to Portal.

--- geoportal-commons/geoportal-commons-agp-client/src/main/java/com/esri/geoportal/commons/agp/client/AgpClient.java
+++ geoportal-commons/geoportal-commons-agp-client/src/main/java/com/esri/geoportal/commons/agp/client/AgpClient.java
@@ -49,13 +49,20 @@ import org.apache.http.client.methods.HttpRequestWrapper;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.client.utils.URIUtils;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.entity.mime.content.StringBody;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.message.BasicNameValuePair;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * ArcGIS Portal client.
  */
 public class AgpClient implements Closeable {
+  private static final Logger LOG = LoggerFactory.getLogger(AgpClient.class);
   private static final String QUERY_EXTRAS = "-type:\"Layer\" -type: \"Map Document\" -type:\"Map Package\" -type:\"Basemap Package\" -type:\"Mobile Basemap Package\" -type:\"Mobile Map Package\" -type:\"ArcPad Package\" -type:\"Project Package\" -type:\"Project Template\" -type:\"Desktop Style\" -type:\"Pro Map\" -type:\"Layout\" -type:\"Explorer Map\" -type:\"Globe Document\" -type:\"Scene Document\" -type:\"Published Map\" -type:\"Map Template\" -type:\"Windows Mobile Package\" -type:\"Layer Package\" -type:\"Explorer Layer\" -type:\"Geoprocessing Package\" -type:\"Desktop Application Template\" -type:\"Code Sample\" -type:\"Geoprocessing Package\" -type:\"Geoprocessing Sample\" -type:\"Locator Package\" -type:\"Workflow Manager Package\" -type:\"Windows Mobile Package\" -type:\"Explorer Add In\" -type:\"Desktop Add In\" -type:\"File Geodatabase\" -type:\"Feature Collection Template\" -type:\"Code Attachment\" -type:\"Featured Items\" -type:\"Symbol Set\" -type:\"Color Set\" -type:\"Windows Viewer Add In\" -type:\"Windows Viewer Configuration\"";
   private static final Integer DEFAULT_MAX_REDIRECTS = 5;
   
@@ -105,14 +112,17 @@ public class AgpClient implements Closeable {
    * @throws IOException if operation fails
    */
   public ItemResponse addItem(String owner, String folderId, String title, String description, URL url, URL thumbnailUrl, ItemType itemType, Double [] extent, String [] typeKeywords, String [] tags, File fileToUpload, String token) throws IOException, URISyntaxException {
+    boolean multipart = fileToUpload!=null;
     URIBuilder builder = new URIBuilder(addItemUri(owner, StringUtils.trimToNull(folderId)));
     
     HttpPost req = new HttpPost(builder.build());
     
     Map<String, String> params = makeStdParams(title, description, itemType, thumbnailUrl, extent, typeKeywords, tags, token);
-    params.put(itemType.getDataType().name().toLowerCase(), url.toExternalForm());
+    if (!multipart) {
+      params.put(itemType.getDataType().name().toLowerCase(), url.toExternalForm());
+    }
     
-    req.setEntity(createEntity(params));
+    req.setEntity(multipart? createEntity(params, fileToUpload): createEntity(params));
 
     return execute(req,ItemResponse.class);
   }
@@ -137,14 +147,17 @@ public class AgpClient implements Closeable {
    * @throws IOException if operation fails
    */
   public ItemResponse updateItem(String owner, String folderId, String itemId, String title, String description, URL url, URL thumbnailUrl, ItemType itemType, Double [] extent, String [] typeKeywords, String [] tags, File fileToUpload, String token) throws IOException, URISyntaxException {
+    boolean multipart = fileToUpload!=null;
     URIBuilder builder = new URIBuilder(updateItemUri(owner, StringUtils.trimToNull(folderId), itemId));
     
     HttpPost req = new HttpPost(builder.build());
     
     Map<String, String> params = makeStdParams(title, description, itemType, thumbnailUrl, extent, typeKeywords, tags, token);
-    params.put(itemType.getDataType().name().toLowerCase(), url.toExternalForm());
+    if (!multipart) {
+      params.put(itemType.getDataType().name().toLowerCase(), url.toExternalForm());
+    }
     
-    req.setEntity(createEntity(params));
+    req.setEntity(multipart? createEntity(params, fileToUpload): createEntity(params));
 
     return execute(req,ItemResponse.class);
   }
@@ -165,7 +178,9 @@ public class AgpClient implements Closeable {
    * @return add item response
    * @throws URISyntaxException if invalid URL
    * @throws IOException if operation fails
+   * @deprecated to be removed without substitution
    */
+  @Deprecated
   public ItemResponse addItem(String owner, String folderId, String title, String description, String text, URL thumbnailUrl, ItemType itemType, Double [] extent, String [] typeKeywords, String [] tags, String token) throws IOException, URISyntaxException {
     URIBuilder builder = new URIBuilder(addItemUri(owner, StringUtils.trimToNull(folderId)));
     
@@ -196,7 +211,9 @@ public class AgpClient implements Closeable {
    * @return add item response
    * @throws URISyntaxException if invalid URL
    * @throws IOException if operation fails
+   * @deprecated to be removed without substitution
    */
+  @Deprecated
   public ItemResponse updateItem(String owner, String folderId, String itemId, String title, String description, String text, URL thumbnailUrl, ItemType itemType, Double [] extent, String [] typeKeywords, String [] tags, String token) throws IOException, URISyntaxException {
     URIBuilder builder = new URIBuilder(updateItemUri(owner, StringUtils.trimToNull(folderId), itemId));
     
@@ -208,29 +225,6 @@ public class AgpClient implements Closeable {
     req.setEntity(createEntity(params));
 
     return execute(req,ItemResponse.class);
-  }
-  
-  private Map<String, String> makeStdParams(String title, String description, ItemType itemType, URL thumbnailUrl, Double [] extent, String [] typeKeywords, String [] tags, String token) {
-    HashMap<String, String> params = new HashMap<>();
-    params.put("f", "json");
-    params.put("title", title);
-    params.put("description", description);
-    params.put("type", itemType.getTypeName());
-    if (thumbnailUrl!=null) {
-      params.put("thumbnailurl", thumbnailUrl.toExternalForm());
-    }
-    if (extent!=null && extent.length==4) {
-      params.put("extent",Arrays.asList(extent).stream().map(Object::toString).collect(Collectors.joining(",")));
-    }
-    if (typeKeywords!=null) {
-      params.put("typeKeywords", Arrays.asList(typeKeywords).stream().collect(Collectors.joining(",")));
-    }
-    if (tags!=null) {
-      params.put("tags", Arrays.asList(tags).stream().collect(Collectors.joining(",")));
-    }
-    params.put("token", token);
-    
-    return params;
   }
 
   /**
@@ -449,6 +443,29 @@ public class AgpClient implements Closeable {
     return execute(req,TokenResponse.class);
   }
   
+  private Map<String, String> makeStdParams(String title, String description, ItemType itemType, URL thumbnailUrl, Double [] extent, String [] typeKeywords, String [] tags, String token) {
+    HashMap<String, String> params = new HashMap<>();
+    params.put("f", "json");
+    params.put("title", title);
+    params.put("description", description);
+    params.put("type", itemType.getTypeName());
+    if (thumbnailUrl!=null) {
+      params.put("thumbnailurl", thumbnailUrl.toExternalForm());
+    }
+    if (extent!=null && extent.length==4) {
+      params.put("extent",Arrays.asList(extent).stream().map(Object::toString).collect(Collectors.joining(",")));
+    }
+    if (typeKeywords!=null) {
+      params.put("typeKeywords", Arrays.asList(typeKeywords).stream().collect(Collectors.joining(",")));
+    }
+    if (tags!=null) {
+      params.put("tags", Arrays.asList(tags).stream().collect(Collectors.joining(",")));
+    }
+    params.put("token", token);
+    
+    return params;
+  }
+  
   private URI itemInfoUri(String itemId) throws URISyntaxException {
     URIBuilder builder = new URIBuilder();
     builder.setScheme(rootUrl.toURI().getScheme())
@@ -475,7 +492,7 @@ public class AgpClient implements Closeable {
            .setPath(rootUrl.toURI().getPath() + "sharing/rest/content/users/" + owner + (folderId!=null? "/" +folderId: "") +"/items/" + itemId + "/update");
     return builder.build();
   }
-
+  
   private URI addItemUri(String owner, String folderId) throws URISyntaxException {
     URIBuilder builder = new URIBuilder();
     builder.setScheme(rootUrl.toURI().getScheme())
@@ -531,6 +548,14 @@ public class AgpClient implements Closeable {
             .map(e -> new BasicNameValuePair(e.getKey(), e.getValue())).collect(Collectors.toList()), "UTF-8");
   }
   
+  private HttpEntity createEntity(Map<String, String> params, File file) throws UnsupportedEncodingException {
+    MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+    builder.setMode(HttpMultipartMode.RFC6532);
+    params.entrySet().stream().forEach(e->builder.addPart(e.getKey(), new StringBody(e.getValue(), ContentType.MULTIPART_FORM_DATA)));
+    builder.addBinaryBody("file", file);
+    return builder.build();
+  }
+  
   private <T> T execute(HttpUriRequest req, Class<T> clazz) throws IOException {
     String responseContent = execute(req, 0);
     ObjectMapper mapper = new ObjectMapper();
@@ -573,8 +598,8 @@ public class AgpClient implements Closeable {
             newReq.setURI(redirUrl);
 
             return execute(newReq, ++redirectDepth);
-          } catch (Exception e) {
-            e.printStackTrace();
+          } catch (IOException | URISyntaxException e) {
+            LOG.debug("Error executing request", e);
             throw new HttpResponseException(httpResponse.getStatusLine().getStatusCode(), httpResponse.getStatusLine().getReasonPhrase());
           }
         }
