6263acec1c0995cb82cc362091f4c69afd9c6b92
pandzel <pandzel@esri.com>
2019-01-24 11:17:38 -0800

    Portal output broker uploads files by default.

--- geoportal-connectors/geoportal-harvester-agp-publisher/src/main/java/com/esri/geoportal/harvester/agp/AgpOutputConnector.java
+++ geoportal-connectors/geoportal-harvester-agp-publisher/src/main/java/com/esri/geoportal/harvester/agp/AgpOutputConnector.java
@@ -78,7 +78,7 @@ public class AgpOutputConnector implements OutputConnector<OutputBroker> {
     });
     args.add(new UITemplate.IntegerArgument(P_MAX_REDIRECTS, bundle.getString("agp.max.redirects"), false, 5));
     args.add(new UITemplate.BooleanArgument(P_FOLDER_CLEANUP, bundle.getString("agp.cleanup")));
-    args.add(new UITemplate.BooleanArgument(P_UPLOAD, bundle.getString("agp.upload")));
+    args.add(new UITemplate.BooleanArgument(P_UPLOAD, bundle.getString("agp.upload"), true, true));
     return new UITemplate(getType(), bundle.getString("agp"), args);
   }
   
