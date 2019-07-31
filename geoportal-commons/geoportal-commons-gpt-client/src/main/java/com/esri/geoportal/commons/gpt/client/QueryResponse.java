7570517393ba14871c451c2e53fd6cbc79357b97
pandzel <pandzel@esri.com>
2019-07-31 13:39:36 -0700

    Reading total hits from ES 7.

--- geoportal-commons/geoportal-commons-gpt-client/src/main/java/com/esri/geoportal/commons/gpt/client/QueryResponse.java
+++ geoportal-commons/geoportal-commons-gpt-client/src/main/java/com/esri/geoportal/commons/gpt/client/QueryResponse.java
@@ -15,15 +15,32 @@
  */
 package com.esri.geoportal.commons.gpt.client;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import java.io.IOException;
 import java.util.List;
 
 /**
  * Query response.
  */
 /*package*/ final class QueryResponse {
+  private static final ObjectMapper mapper = new ObjectMapper();
+  static {
+    mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+  }
+    
   public String _scroll_id;
   public Hits hits;
  
+  @JsonDeserialize(using = QueryResponse.HitsDeserializer.class)
   public static final class Hits {
     public long total;
     public List<Hit> hits;
@@ -37,5 +54,29 @@ import java.util.List;
   public static final class Source {
     public String src_uri_s;
     public String src_lastupdate_dt;
+  }
+  
+  public static final class HitsDeserializer extends JsonDeserializer<Hits> {
+
+    @Override
+    public Hits deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException, JsonProcessingException {
+      JsonNode node = jp.getCodec().readTree(jp);
+      Hits hits = new Hits();
+      
+      if (node.has("total")) {
+        if (node.get("total").isNumber()) {
+          hits.total = node.get("total").asInt();
+        } else if (node.get("total").isObject() && node.get("total").has("value") && node.get("total").get("value").isNumber()) {
+          hits.total = node.get("total").get("value").asInt();
+        }
+      }
+      
+      if (node.has("hits")) {
+        JsonNode hitsNode = node.get("hits");
+        hits.hits = mapper.treeToValue(hitsNode, List.class);
+      }
+      return hits;
+    }
+    
   }
 }
