add5ff31a97d17322ee48ca4a38799c7aa5fc78b
pandzel <pandzel@esri.com>
2020-08-18 14:13:52 -0700

    CSW PROFILES: ResourceFolderCopier.

---
+++ geoportal-commons/geoportal-commons-csw-client/src/main/java/com/esri/geoportal/commons/csw/client/impl/ResourceFolderCopier.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2020 Esri, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.esri.geoportal.commons.csw.client.impl;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.commons.io.FileUtils;
+
+/**
+ * Resource folder copier.
+ */
+public class ResourceFolderCopier {
+  private static final Logger LOG = Logger.getLogger(ResourceFolderCopier.class.getName());
+  
+  public static void copy(String resourceFolder, File destinationFolder) {
+    ClassLoader loader = Thread.currentThread().getContextClassLoader();
+    URL url = loader.getResource(Constants.CONFIG_FOLDER_PATH);
+    String path = url.getPath();
+    File[] files = new File(path).listFiles();
+
+    if (files != null) {
+      destinationFolder.mkdirs();
+      
+      Arrays.stream(files).forEach(f -> {
+        try {
+          FileUtils.copyToDirectory(f, destinationFolder);
+        } catch (IOException ex) {
+          LOG.log(Level.FINE, String.format("Error copying %s to %s", f, destinationFolder), ex);
+        }
+      });
+    }
+  }
+}
