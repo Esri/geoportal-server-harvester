500c9ffc4b1045cf3104ea8c36f303fb89206fa5
cont_anki <ankita_srivastava@esri.com>
2023-11-01 11:35:46 +0530

    Revert "update terminate function to count actual deletes"
    This reverts commit 8d4597f3f089ed5bafd431a55934fda3ddc1be59.

--- geoportal-connectors/geoportal-harvester-gpt/src/main/java/com/esri/geoportal/harvester/gpt/GptBroker.java
+++ geoportal-connectors/geoportal-harvester-gpt/src/main/java/com/esri/geoportal/harvester/gpt/GptBroker.java
@@ -47,9 +47,7 @@ import com.esri.geoportal.harvester.api.ex.DataOutputException;
 import com.esri.geoportal.harvester.api.ex.DataProcessorException;
 import com.esri.geoportal.harvester.api.specs.OutputBroker;
 import com.esri.geoportal.harvester.api.specs.OutputConnector;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.ListIterator;
 import java.util.stream.Collectors;
 import org.apache.commons.lang3.StringUtils;
 
@@ -124,16 +122,10 @@ import org.apache.commons.lang3.StringUtils;
   public void terminate() {
     try {
       if (client != null && definition.getCleanup() && !preventCleanup) {
-        int deleted = 0;
-        
         for (String id : existing) {
-          PublishResponse deleteResult = client.delete(id);
-          if ((deleteResult != null) && (deleteResult.getStatus() != null)) {
-            deleted += 1;
-          }
-        }            
-        
-        LOG.info(String.format("%d records has been removed during cleanup.", deleted));
+          client.delete(id);
+        }
+        LOG.info(String.format("%d records has been removed during cleanup.", existing.size()));
       }
     } catch (URISyntaxException | IOException ex) {
       LOG.error(String.format("Error terminating broker."), ex);
