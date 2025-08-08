008608e104f0a959ed4df306b6a2777c36077fb1
cont_anki <ankita_srivastava@esri.com>
2025-08-08 15:21:21 +0530

    CodeQL Issues

--- geoportal-commons/geoportal-commons-meta/src/main/java/com/esri/geoportal/commons/meta/xml/BaseXmlMetaBuilder.java
+++ geoportal-commons/geoportal-commons-meta/src/main/java/com/esri/geoportal/commons/meta/xml/BaseXmlMetaBuilder.java
@@ -19,9 +19,9 @@ import com.esri.geoportal.commons.meta.AttributeUtils;
 import com.esri.geoportal.commons.meta.MetaBuilder;
 import com.esri.geoportal.commons.meta.MetaException;
 import com.esri.geoportal.commons.meta.MapAttribute;
+import static com.esri.geoportal.commons.meta.xml.TransformerLoader.loadTransformer;
 import java.io.IOException;
 import java.util.Properties;
-import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Result;
@@ -29,7 +29,6 @@ import javax.xml.transform.Templates;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMResult;
 import javax.xml.transform.dom.DOMSource;
 import org.w3c.dom.Document;
@@ -48,11 +47,7 @@ public abstract class BaseXmlMetaBuilder implements MetaBuilder {
    * @throws javax.xml.transform.TransformerConfigurationException if error compiling xslt
    */
   public BaseXmlMetaBuilder(String encoderXslt) throws IOException, TransformerConfigurationException {
-      TransformerFactory factory = TransformerFactory.newInstance();
-      factory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
-      factory.setAttribute(XMLConstants.ACCESS_EXTERNAL_DTD, "");
-      factory.setAttribute(XMLConstants.ACCESS_EXTERNAL_STYLESHEET, "");
-      xsltEncodeDC = factory.newTemplates(new javax.xml.transform.stream.StreamSource(encoderXslt));
+    xsltEncodeDC = loadTransformer(encoderXslt);
   }
 
   @Override
