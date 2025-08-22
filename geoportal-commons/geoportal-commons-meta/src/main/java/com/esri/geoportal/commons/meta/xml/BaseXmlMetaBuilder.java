28d443d2c5001f1b4f923bddc49439a57bea5944
cont_anki <ankita_srivastava@esri.com>
2025-08-22 14:07:08 +0530

    CodeQL-Issue- 30

--- geoportal-commons/geoportal-commons-meta/src/main/java/com/esri/geoportal/commons/meta/xml/BaseXmlMetaBuilder.java
+++ geoportal-commons/geoportal-commons-meta/src/main/java/com/esri/geoportal/commons/meta/xml/BaseXmlMetaBuilder.java
@@ -19,27 +19,19 @@ import com.esri.geoportal.commons.meta.AttributeUtils;
 import com.esri.geoportal.commons.meta.MetaBuilder;
 import com.esri.geoportal.commons.meta.MetaException;
 import com.esri.geoportal.commons.meta.MapAttribute;
+import static com.esri.geoportal.commons.meta.xml.TransformerLoader.loadTransformer;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.StringReader;
 import java.util.Properties;
-import javax.xml.XMLConstants;
-import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Result;
-import javax.xml.transform.Source;
 import javax.xml.transform.Templates;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.URIResolver;
 import javax.xml.transform.dom.DOMResult;
 import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamSource;
 import org.w3c.dom.Document;
-import org.xml.sax.InputSource;
 
 /**
  * Base xml meta builder
@@ -47,9 +39,7 @@ import org.xml.sax.InputSource;
 public abstract class BaseXmlMetaBuilder implements MetaBuilder {
 
   private final Templates xsltEncodeDC;
-    static final URIResolver DENY_ALL_URI_RESOLVER = (href, base) -> {
-        throw new TransformerException("External resource access is blocked");
-    };
+  
   /**
    * Creates instance of the builder.
    * @param encoderXslt decoder xslt
@@ -57,44 +47,20 @@ public abstract class BaseXmlMetaBuilder implements MetaBuilder {
    * @throws javax.xml.transform.TransformerConfigurationException if error compiling xslt
    */
   public BaseXmlMetaBuilder(String encoderXslt) throws IOException, TransformerConfigurationException {
-    try (final InputStream input = Thread.currentThread().getContextClassLoader().getResourceAsStream(encoderXslt)) {      
-      TransformerFactory transFact = TransformerFactory.newInstance();
-      transFact.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
-      transFact.setAttribute(XMLConstants.ACCESS_EXTERNAL_DTD, "");
-      transFact.setAttribute(XMLConstants.ACCESS_EXTERNAL_STYLESHEET, "");
-      Source source = new StreamSource(input);
-      xsltEncodeDC= transFact.newTemplates(source);
-    }
+    xsltEncodeDC = loadTransformer(encoderXslt);
   }
-  
 
   @Override
   public Document create(MapAttribute wellKnowsAttributes) throws MetaException {
     try {
-        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-        dbf.setNamespaceAware(true);
-
-        // Harden the parser
-        dbf.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
-        try { dbf.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true); } catch (Exception ignored) {}
-        try { dbf.setFeature("http://xml.org/sax/features/external-general-entities", false); } catch (Exception ignored) {}
-        try { dbf.setFeature("http://xml.org/sax/features/external-parameter-entities", false); } catch (Exception ignored) {}
-        dbf.setXIncludeAware(false);
-        dbf.setExpandEntityReferences(false);
-        // Block external DTD/schema fetches (supported on modern JAXP)
-        try { dbf.setAttribute(XMLConstants.ACCESS_EXTERNAL_DTD, ""); } catch (IllegalArgumentException ignored) {}
-        try { dbf.setAttribute(XMLConstants.ACCESS_EXTERNAL_SCHEMA, ""); } catch (IllegalArgumentException ignored) {}
-     
-      Document inputDoc = dbf.newDocumentBuilder().newDocument();
-      
+      Document inputDoc = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
       Document outputDoc = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
-      Transformer transformer = xsltEncodeDC.newTransformer();     
+      Transformer transformer = xsltEncodeDC.newTransformer();
       Properties props = AttributeUtils.toProperties(wellKnowsAttributes);
       props.keySet().stream().map(Object::toString).forEach((String key) -> {
         transformer.setParameter(key, props.getProperty(key));
       });
       Result result = new DOMResult(outputDoc);
-     
       transformer.transform(new DOMSource(inputDoc), result);
       return outputDoc;
     } catch (ParserConfigurationException | TransformerException ex) {
