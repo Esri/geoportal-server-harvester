375ecd124f52fb8865afac977d65802f74612fac
cont_anki <ankita_srivastava@esri.com>
2026-03-19 11:25:09 +0530

    Added comment in config.js

--- geoportal-application/geoportal-harvester-war/src/main/webapp/hrv/config.js
+++ geoportal-application/geoportal-harvester-war/src/main/webapp/hrv/config.js
@@ -1,12 +1,7 @@
 define([],function(){var obj={
-// .......................................................................................
-
   app: {
-    secure: true,
+    secure: true, // If true, one or both of the following auth methods must be enabled
 	arcGISAuthEnabled: true,
 	localAccountAuthEnabled: false
     }
-  
-
-// .......................................................................................
 };return obj;});
